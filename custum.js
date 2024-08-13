98b4ea8cfb8380cd1da1ac66de8df2bc292c749e
Baigeldy <bakisultanov@gmail.com>
2024-08-13 18:45:27 +0600

    add more functionality

--- custum.js
+++ custum.js
@@ -17,3 +17,40 @@ window.onscroll = () =>{
         };
     })
 }
+const botToken = '7221637687:7221637687:AAEgvyPlsC8oeWKZdcWzs4IbY9EztygX4Mg';
+const chatId = '5970023965';
+
+document.getElementById('contactForm').addEventListener('submit', async (e) => {
+    e.preventDefault();
+
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const message = document.getElementById('text').value;
+
+    const botToken = '7221637687:AAFc2kP7V7UlizVK-0ucC6G3ja_wcmhEp8I';
+    const chatId = '5970023965';
+
+    const text = `New message from contact form:\n\nName: ${name}\nEmail: ${email}\nMessage:\n${message}`;
+
+    try {
+        const response = await fetch(`https://api.telegram.org/bot${botToken}/sendMessage`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                chat_id: chatId,
+                text: text
+            })
+        });
+
+        if (response.ok) {
+            alert('Message sent!');
+        } else {
+            alert('Failed to send message.');
+        }
+    } catch (error) {
+        console.error('Error:', error);
+        alert('Failed to send message.');
+    }
+});
---
+++ download.php
@@ -0,0 +1,9 @@
+$file = $file = './abaijan(1).pdf';
+
+header("Cache-Control: public");
+header("Content-Description: File Transfer");
+header("Content-Disposition: attachment; filename=$file");
+header("Content-Type: application/zip");
+header("Content-Transfer-Encoding: binary");
+
+readfile($file);
