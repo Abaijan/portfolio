c23ee487ffd2cbd38bcd7e387fcbd578e5494235
Baigeldy <bakisultanov@gmail.com>
2024-08-13 23:37:40 +0600

    last commit fixed custom json

--- custum.js
+++ custum.js
@@ -29,28 +29,30 @@ document.getElementById('contactForm').addEventListener('submit', async (e) => {
 
     const botToken = '7221637687:AAFc2kP7V7UlizVK-0ucC6G3ja_wcmhEp8I';
     const chatId = '5970023965';
+    if(name !== '' || email !== '') {
+        const text = `New message from contact form:\n\nName: ${name}\nEmail: ${email}\nMessage:\n${message}`;
 
-    const text = `New message from contact form:\n\nName: ${name}\nEmail: ${email}\nMessage:\n${message}`;
-
-    try {
-        const response = await fetch(`https://api.telegram.org/bot${botToken}/sendMessage`, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                chat_id: chatId,
-                text: text
-            })
-        });
-
-        if (response.ok) {
-            alert('Message sent!');
-        } else {
+        try {
+            const response = await fetch(`https://api.telegram.org/bot${botToken}/sendMessage`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    chat_id: chatId,
+                    text: text
+                })
+            });
+    
+            if (response.ok) {
+                alert('Message sent!');
+            } else {
+                alert('Failed to send message.');
+            }
+        } catch (error) {
+            console.error('Error:', error);
             alert('Failed to send message.');
         }
-    } catch (error) {
-        console.error('Error:', error);
-        alert('Failed to send message.');
-    }
+    } else alert ("Sorry write something")
+   
 });
